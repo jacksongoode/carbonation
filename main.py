df45b98491284998d2d42490c3f6ff36a963c6de
Jackson Goode <jacksongoode@proton.me>
2022-11-26 23:13:57 -0800

    Initial fastapi

---
+++ main.py
@@ -0,0 +1,84 @@
+import argparse
+import json
+import os
+
+import uvicorn
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from newsapi import NewsApiClient
+from newscatcherapi import NewsCatcherApiClient
+
+from utils import get_newsapi, get_newscatcher_headlines, split_url
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+# app.jinja_env.globals.update(split_url=split_url)
+
+# set configuration values
+load_dotenv()
+catcher = NewsCatcherApiClient(x_api_key=os.environ.get("NEWSCATCHER"))
+newsapi = NewsApiClient(api_key=os.environ.get("NEWSAPI"))
+
+# Routing
+@app.get("/", response_class=HTMLResponse)
+@app.get("/about", response_class=HTMLResponse)
+def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
+
+
+@app.get("/newsapi", response_class=HTMLResponse)
+def render_newsapi(request: Request):
+    news = get_newsapi("*")
+    data = {
+        "articles": news["articles"],
+        "total_results": news["totalResults"],
+    }
+
+    return templates.TemplateResponse(
+        "newsapi.html", {"request": request, "data": data}
+    )
+
+
+@app.get("/newscatcher", response_class=HTMLResponse)
+def render_newscatcher(request: Request):
+    news = get_newscatcher_headlines(1)
+    data = {"articles": news.get("articles", ""), "total_results": news["total_hits"]}
+
+    return templates.TemplateResponse(
+        "newscatcher.html", {"request": request, "data": data}
+    )
+
+
+@app.get("/model", response_class=HTMLResponse)
+def render_model(request: Request):
+    model_fn = "bert_test.json"
+    with open(f"resources/computed/{model_fn}", "r") as f:
+        data = json.load(f)
+
+    return templates.TemplateResponse("bubble.html", {"request": request, "data": data})
+
+
+if __name__ == "__main__":
+    # Parse arguments
+    parser = argparse.ArgumentParser(prog="carbonation", description="Make news!")
+    parser.add_argument("-d", "--debug", action="store_true")
+    args = parser.parse_args()
+
+    # if not args.debug:
+    #     from analysis import generate_bert
+
+    #     print("Starting scheduler!")
+    #     scheduler.start()
+    #     scheduler.add_job(
+    #         id="generate_bert",
+    #         func=generate_bert,
+    #         trigger="interval",
+    #         hours=4,
+    #     )
+
+    print("Starting!")
+    uvicorn.run("main:app", port=5000, log_level="info")
--- utils/fetch.py
+++ utils/fetch.py
@@ -1,6 +1,6 @@
-
-import os
 import json
+import os
+from urllib.parse import urlparse
 
 import requests
 from dotenv import load_dotenv
@@ -8,8 +8,8 @@ from newsapi import NewsApiClient
 from newscatcherapi import NewsCatcherApiClient
 
 load_dotenv()
-catcher = NewsCatcherApiClient(x_api_key=os.environ.get('NEWSCATCHER'))
-newsapi = NewsApiClient(api_key=os.environ.get('NEWSAPI'))
+catcher = NewsCatcherApiClient(x_api_key=os.environ.get("NEWSCATCHER"))
+newsapi = NewsApiClient(api_key=os.environ.get("NEWSAPI"))
 
 
 def download_resource(url, path):
@@ -20,14 +20,14 @@ def download_resource(url, path):
 
 
 def fetch_newscatcher_sources():
-    sources = catcher.get_sources(lang='en', countries='US')
-    with open('resources/sources/newscatcher.json', 'w') as f:
+    sources = catcher.get_sources(lang="en", countries="US")
+    with open("resources/sources/newscatcher.json", "w") as f:
         json.dump(sources, f)
 
 
 def fetch_newsapi_sources():
-    sources = newsapi.get_sources(language='en')
-    with open('resources/sources/newsapi.json', 'w') as f:
+    sources = newsapi.get_sources(language="en")
+    with open("resources/sources/newsapi.json", "w") as f:
         json.dump(sources, f)
 
 
@@ -39,3 +39,22 @@ def fetch_mbfc():
 def fetch_allsides():
     allsides = "https://www.allsides.com/media-bias/json/noncommercial/all"
     download_resource(allsides, "bias/allsides_bias")
+
+
+def write_bias():
+    # Get urls
+    with open("resources/bias/allsides_bias.json", "r") as f:
+        allsides = json.load(f)["allsides_media_bias_ratings"]
+    as_urls = [s["source_url"] for s in allsides if s["source_url"] != ""]
+    as_urls = [".".join(urlparse(url).netloc.split(".")[-2:]) for url in as_urls]
+
+    with open("resources/bias/mbfc_bias.json", "r") as f:
+        mbfc = json.load(f)
+    mbfc_urls = list(mbfc.keys())
+    mbfc_urls = [u for u in mbfc_urls if "/" not in u]
+
+    urls = list(set(as_urls + mbfc_urls))
+
+    with open("resources/sources/sources.txt", "w") as f:
+        for line in urls:
+            f.write(f"{line}\n")

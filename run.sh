47719156149ac940d084fc6cb412f597af2e09c8
Jackson Goode <jacksongoode@proton.me>
2022-11-28 13:20:18 -0800

    Add methods

--- run.sh
+++ run.sh
@@ -5,7 +5,10 @@ case $1 in
 		npm run sass-dev & uvicorn --reload --reload-include="*.css" main:app
 		;;
 	prod)
-		npm run sass-prod
+		npm run sass-prod && uvicorn --host 0.0.0.0 --port 8080 main:app
+		;;
+	railway)
+		npm run sass-prod && uvicorn --host 0.0.0.0 --port $PORT main:app
 		;;
 	*)
 		echo "Unknown method!"
